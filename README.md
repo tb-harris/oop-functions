0a07bae8755c0fe9de21ae2887d9d549c3770243
tb-harris <tbrandonharris@riverdale.edu>
2025-04-29 11:05:40 -0400

    finished

---
+++ README.md
@@ -0,0 +1,26 @@
+# Methods Practice
+
+## Main.java
+Define the following functions.
+
+After defining each function, uncomment the corresponding function calls in main() and run your code to test.
+
+* triangleArea(base, height) - returns the area of a triangle given its base and height
+* cube(n) - returns the cube of n
+* max3(a, b, c) - returns the maximum of three numbers
+* factorial(n) - returns the factorial of n (n * (n-1) * (n-2) * ... * 1)
+* min(arr) - returns the minimum value in an array
+* chooseRandom(arr) - returns a random element from an array
+
+## Bonus.java
+Define the following functions, **and add calls to test them**:
+* isEven(n) - returns true if n is even, false otherwise
+* nextMultiple(n, m) - returns the next multiple of m after n
+* sort(arr) - Returns a sorted version of an array of integers
+
+
+## Recursion.java
+Read about [recursion](https://www.geeksforgeeks.org/recursion-in-java/), and implement the following functions **recursively**:
+
+* factorial(n) - Returns the factorial of n
+* product(numbers) - Returns the product of the array
---
+++ src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+        // triangleArea(base, height)
+        /*
+        System.out.println("Triangle area with base 5 and height 3 is " + triangleArea(5, 3));
+        System.out.println("Triangle area with base 10 and height 4 is " + triangleArea(10.5, 4));
+        */
+
+
+        // cube(n)
+        /*
+        System.out.println("2^3 is " + cube(2));
+        System.out.println("4^3 is " + cube(4));
+        System.out.println("9^3 is " + cube(9));
+        System.out.println("0.5^3 is " + cube(0.5));
+        */
+
+        // max3(a, b, c)
+        /*
+        System.out.println("Max of 5, 3.14, 8 is " + max3(5, 3.14, 8));
+        System.out.println("Max of 20.15, 10, 15 is " + max3(20.15, 10, 15));
+        System.out.println("Max of 7, 7, 6 is " + max3(7, 7, 6));
+
+         */
+
+        // factorial(n)
+        /*
+        System.out.println("3*2*1 is " + factorial(3));
+        System.out.println("5*4*3*2*1 is " + factorial(5));
+
+        double[] temps = { 83.2, 72.4, 53.3, 90.1, 88.0, 76.5, 95.9 };
+        double[] grades = { 85.5, 90.0, 78.5, 88.0, 92.5, 80.0, 95.0 };
+         */
+
+        // min(arr)
+        /*
+        System.out.println("The lowest temperature was " + min(temps));
+        System.out.println("The lowest grade was " + min(grades));
+         */
+
+        // chooseRandom(arr)
+        /*
+        System.out.println("Random temperature: " + chooseRandom(temps));
+        System.out.println("Random grade: " + chooseRandom(grades));
+         */
+    }
+}
